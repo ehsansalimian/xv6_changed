64da3d9701d105a06bf697521e4620b75395d17a
ehsan salimian <1372ehsanew@gmail.com>
2019-12-17 21:53:32 +0330

    The getChildren  System Call is added

---
+++ myprogram.c
@@ -0,0 +1,16 @@
+// A simple program which just prints something on screen
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+	
+	
+	  printf(1, "ehsan's first user program on xv6\n");
+ 
+ 
+ exit();
+}
